56948d9ef99567364f1ed34b3a0b731bfcfab534
Matthew Frisby <matt21@live.unc.edu>
2019-02-27 17:51:58 -0500

    Added Angular Form

--- SNC-FE/src/components/home/home.component.ts
+++ SNC-FE/src/components/home/home.component.ts
@@ -5,6 +5,8 @@ import { OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { map } from 'rxjs/operators';
 import { Observable, BehaviorSubject } from 'rxjs';
+import { TestService } from '@services/test.service';
+
 
 
 
@@ -17,7 +19,7 @@ import { Observable, BehaviorSubject } from 'rxjs';
 @Component({
   selector: 'home-root',
   templateUrl: './home.component.html',
-  providers: [],
+  providers: [TestService],
 })
 
 
@@ -25,20 +27,29 @@ export class Home implements OnInit {
 
   constructor(
     private router: Router,
-    private formBuilder: FormBuilder
-
+    private formBuilder: FormBuilder,
+    private skeleService: TestService
     //private home: Home,
     //private account: Account,
   ) { }
 
   create: Boolean = false;
   get: Boolean = false;
+  skeleForm: FormGroup;
 
-
+  questions = ["5c73424df44fb900174f5720","5c73424df44fb900174f572", "5c73424df44fb900174f5722"];
 
   ngOnInit() {
+    this.skeleForm = this.formBuilder.group({
+      Q1: ['', Validators.required],
+      Q2: ['', Validators.required],
+      Q3: ['', Validators.required]
+    });
+
 
   }
+  get f() { return this.skeleForm.controls; }
+
 
   createState(){
     this.create = true;
@@ -55,6 +66,17 @@ export class Home implements OnInit {
     this.create = false;
     this.get = false;
 
+  }
+
+  onSubmit() {
+    for(var i = 1; i<4; i++){
+      var question = 'Q'+i.toString()
+
+
+
+      this.skeleService.updateAnswers(this.questions[i-1],  this.skeleForm.controls[question].value).subscribe(data=>{console.log(data.answer)});
+
+    }
   }
 
 
--- SNC-FE/src/services/test.service.ts
+++ SNC-FE/src/services/test.service.ts
@@ -1,1 +1,37 @@
-//s
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+import { Observable, BehaviorSubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
+
+
+
+@Injectable()
+export class TestService  {
+
+private _url: string = "TEMP"
+//"https://beam-lasercutter.herokuapp.com/api/lasercutter"
+//private _url: string = "http://localhost:3000/api/lasercutter"
+
+  constructor(private http: HttpClient) { }
+
+  updateAnswers(_id: String, answer: String): Observable<any>{
+    const headers = new HttpHeaders({'Content-Type': 'Content-Type: application/json'});
+    return this.http.put<any>(this._url+"/api/demo/"+_id,{Answer: answer});
+  }
+
+  // getQueueAtLocation(location: string): Observable<Response>{
+  //   const headers = new HttpHeaders({'Content-Type': 'Content-Type: application/json'});
+  //   return this.http.get<Response>(this._url+'/'+location);
+  // }
+
+
+
+
+
+
+
+
+}

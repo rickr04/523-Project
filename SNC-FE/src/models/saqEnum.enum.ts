1168d2e95de259502189f0478e57896dd309c03d
Matthew Frisby <matt21@live.unc.edu>
2019-04-10 11:38:53 -0400

    all forms

--- SNC-FE/src/models/saqEnum.enum.ts
+++ SNC-FE/src/models/saqEnum.enum.ts
@@ -1,12 +1,12 @@
 export enum SAQEnum{
-A = "SAQAv3",
-AEP = "",
-B = "",
-BIP="",
-CVT="",
-C = "",
-PPE = "",
-DMERCHANT = "",
-DSERVICE= ""
+A = "SAQAv3_2_1",
+AEP = "SAQA_EPv3_2_1",
+B = "SAQBv3_2_1",
+BIP="SAQB_IPv3_2_1",
+CVT="SAQC_VTv3_2_1",
+C = "SAQCv3_2_1",
+PPE = "SAQP2Pv3_2_1",
+DMERCHANT = "SAQDMv3_2_1",
+DSERVICE= "SAQDSv3_2_1"
 
 }
